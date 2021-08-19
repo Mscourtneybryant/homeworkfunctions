0d7faf58223c116a1b29e433e3baac95fe29d12a
Mscourtneybryant <87495361+Mscourtneybryant@users.noreply.github.com>
2021-08-19 00:18:26 -0400

    Update sum.js
    made changes

--- sum.js
+++ sum.js
@@ -1,16 +1,20 @@
 
     
-let arr = ([2,4,6,8]);
-let startingSum = 0;
+
+
+    let arr = [1,2,3,4];
+
 
 function sumArray(startingSum){
 
 
- for(let i = 0; i > arr.length; i++)
- startingSum[i] = arr
- 
+    
 
-  return arr()
+ for(let i = 0; i < arr.length; i++)
+ startingSum >= arr[i];
+
+
+  return startingSum(arr)
 }
 console.log('The sum is ' + startingSum)
 
