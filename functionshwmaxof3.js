df1c67e6dfe8fcfae54dd6e611df5bc1708ff599
Mscourtneybryant <87495361+Mscourtneybryant@users.noreply.github.com>
2021-08-19 00:17:13 -0400

    Update functionshwmaxof3.js
    made changes

--- functionshwmaxof3.js
+++ functionshwmaxof3.js
@@ -5,10 +5,10 @@ var n3 = (prompt("Enter a final number: "))
 maxOfThree(n1,n2,n3)
 
 function maxOfThree(n1,n2,n3){
-    if (n1 > n2,n3){
+    if (n1 > n2 && n1 > n3){
         console.log(n1 + ' is greater than ' + n2 + ' and ' + n3)
     }
-    else if (n1 < n2,n3){
+    else if (n1 < n2 && n1 < n3) {
         console.log(n1 + ' is less than ' + n2 + ' and ' + n3)
     }
     return;
